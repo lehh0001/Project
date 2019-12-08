2073d86429cae62351b0b69488d6777f5d985ed5
zwl <1010614165@qq.com>
2019-12-08 19:21:57 +0800

    音乐播放

---
+++ Music-player/js/index.js
@@ -0,0 +1,20 @@
+// var xml = new XMLHttpRequest()
+// xml.open('GET','./music.json',true)
+// xml.onload = function(){
+//   window.musicList = JSON.parse(xml.responseText)
+// }
+// xml.send();
+
+
+// $.ajax({
+//   url:"./music.json",
+//   type:"GET",
+//   dataType:"json",
+//   success:function(data){
+//     console.log(data);
+//   },
+//   error:function(data){
+//     console.log(获取不到啦);
+//   }
+
+// })
